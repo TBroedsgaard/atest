f6efce29fa668a69b4645345868ab1bc73bbc323
TBroedsgaard <troels@broedsgaard.com>
2013-09-23 20:05:38 +0200

    stuff

---
+++ src/atest/atest/Program.cs.BACKUP.5892.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+<<<<<<< HEAD
+            Console.WriteLine("Weebang, conflict!");
+=======
+            Console.BackgroundColor = ConsoleColor.Yellow;
+>>>>>>> color-background
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Hello World");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/atest/atest/Program.cs.LOCAL.5892.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Weebang, conflict!");
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Hello World");
+
+            Console.ReadLine();
+        }
+    }
+}
