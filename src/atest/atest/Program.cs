da64eea3e63e13148f0090feb5d114eff6ffd136
TBroedsgaard <troels@broedsgaard.com>
2013-10-01 15:29:48 +0200

    honk

--- src/atest/atest/Program.cs
+++ src/atest/atest/Program.cs
@@ -10,7 +10,7 @@ namespace atest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Writing something. Doh. Bla.");
+            Console.WriteLine("Writing something. Doh. Bla. Honk");
             Console.ReadLine();
             Console.WriteLine("You will never get to see this!");
         }
