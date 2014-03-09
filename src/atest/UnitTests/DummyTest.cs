6ad1336c272ac8ececc5e33bcdf158b7aa1f2c87
TBroedsgaard <troels@broedsgaard.com>
2014-03-09 20:45:48 +0100

    add test

---
+++ src/atest/UnitTests/DummyTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DummyTest
+    {
+        [TestMethod]
+        public void BasicSums()
+        {
+            int x = 2 + 2;
+
+            Assert.AreEqual(x, 2 + 2);
+        }
+    }
+}
